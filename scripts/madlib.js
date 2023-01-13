bdf566ac7ce2aeafecefa126a20bc3de32f9ccda
Amardeep Mann <amar2slick@gmail.com>
2023-01-13 10:53:22 -0800

    Mad Lib JS Complete

--- scripts/madlib.js
+++ scripts/madlib.js
@@ -1,15 +1,21 @@
-let timeInput = document.getElementById("timeInput");
+let input1 = document.getElementById("input1");
+let input2 = document.getElementById("input2");
+let input3 = document.getElementById("input3");
+let input4 = document.getElementById("input4");
+let input5 = document.getElementById("input5");
+let input6 = document.getElementById("input6");
+let input7 = document.getElementById("input7");
+let input8 = document.getElementById("input8");
+let input9 = document.getElementById("input9");
+let input10 = document.getElementById("input10");
 let output = document.getElementById("output");
 let submitBtn = document.getElementById("submitBtn");
 
 // console.log(nameInput, helloReturn, helloSubBtn); // Log variables to console
 
-let savedInput = "";
-let sayHelloUrl = "";
-
 submitBtn.addEventListener("click", function(){
     // helloApi(nameInput); // Using Scott's Example
-    ask(timeInput);
+    ask(input1, input2, input3, input4, input5, input6, input7, input8, input9, input10);
 });
 
 function urlCall(url){
@@ -23,12 +29,11 @@ function urlCall(url){
     )
 }
 
-function ask(timeInput) {
-    if(timeInput.value) {
-        savedInput = timeInput.value;
-        sayHelloUrl = "https://allinoneendpoints.azurewebsites.net/minichallenge/madlib/Harman/sick/cop/Weezer/silly/strong/whistle/chocco-tacos/thicc/" + savedInput;
-        urlCall(sayHelloUrl);
+function ask(input1, input2, input3, input4, input5, input6, input7, input8, input9, input10) {
+    if(input1.value && input2.value && input3.value && input4.value && input5.value && input6.value && input7.value && input8.value && input9.value && input10.value) {
+        let url = `https://allinoneendpoints.azurewebsites.net/minichallenge/madlib/${input1.value}/${input2.value}/${input3.value}/${input4.value}/${input5.value}/${input6.value}/${input7.value}/${input8.value}/${input9.value}/${input10.value}`;
+        urlCall(url);
     } else {
-        output.textContent = "Enter a valid response";
+        output.textContent = "Please fill out all text fields";
     }
 }
